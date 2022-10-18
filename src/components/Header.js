0573c33a1d49654b4ee0793537559610d7bb6d6b
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-10-17 22:59:41 -0400

    update gitignore

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { styles } from '../styles/main'
 function Header(){
   return (
     <>
-      <h1>Help Queue</h1>
+      <h1 style={styles.h1Style}>Help Queue</h1>
       <img src={ticketsImage} alt="ticket" style={styles.imgStyle} />
     </>
   );
--- src/styles/main.js
+++ src/styles/main.js
@@ -2,5 +2,12 @@ export const styles = {
   imgStyle: {
     width: '30%',
     height: 'auto'
+  },
+
+  h1Style: {
+    color: 'red',
+    fontSize: '2rem'
   }
 }
+
+
